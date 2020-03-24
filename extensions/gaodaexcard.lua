c95808bc2ae57b91e2f01bb20bcd781d53749959
wch5621628 <wchwchwchw@hotmail.com>
2020-03-24 16:26:26 +0800

    1.7

--- extensions/gaodaexcard.lua
+++ extensions/gaodaexcard.lua
@@ -26,27 +26,27 @@ final_vent = sgs.CreateTrickCard{
 		self:cardOnUse(room, use)
 	end,
 	on_use = function(self, room, source, targets)
-		room:getThread():delay(1000)
-		for _, t in ipairs(targets) do
-			room:cardEffect(self, source, t)
-		end
-		--[[if room:getCardPlace(self:getEffectiveId()) == sgs.Player_PlaceTable then
-			local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_USE, source:objectName(), "", self:getSkillName(), "")
-			room:moveCardTo(self, source, nil, sgs.Player_DiscardPile, reason, true)
-		end]]
-	end,
-	on_effect = function(self, effect)
-		local room = effect.from:getRoom()
-		
 		if room:getCardPlace(self:getEffectiveId()) == sgs.Player_PlaceTable then
-			effect.from:addToPile("#final_vent", self)
+			--移动至隐藏pile，在UI层面上移出游戏，同时防止中途获得此牌（如 “奸雄”）
+			source:addToPile("#final_vent", self)
 			local log = sgs.LogMessage()
 			log.type = "$AddToPile"
 			log.card_str = self:toString()
 			log.arg = "single_target_trick"
 			room:sendLog(log)
+			
+			--移动至PlaceUnknown，在服务器层面上移出游戏，防止AI把隐形牌用作视为技（如 隐形“武圣”【杀】），但再次获得此牌时会闪退（如 作弊卡牌一览）
+			local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_USE, source:objectName(), "", self:getSkillName(), "")
+			room:moveCardTo(self, source, nil, sgs.Player_PlaceUnknown, reason, true)
 		end
 		
+		room:getThread():delay(1000)
+		for _, t in ipairs(targets) do
+			room:cardEffect(self, source, t)
+		end
+	end,
+	on_effect = function(self, effect)
+		local room = effect.from:getRoom()		
 		local x = 1
 		for _,card in sgs.qlist(effect.from:getEquips()) do
 			if card:isKindOf("Horse") and card:isRed() then
@@ -85,27 +85,27 @@ decade = sgs.CreateTrickCard{
 		self:cardOnUse(room, use)
 	end,
 	on_use = function(self, room, source, targets)
-		room:getThread():delay(1700)
-		for _, t in ipairs(targets) do
-			room:cardEffect(self, source, t)
-		end
-		--[[if room:getCardPlace(self:getEffectiveId()) == sgs.Player_PlaceTable then
-			local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_USE, source:objectName(), "", self:getSkillName(), "")
-			room:moveCardTo(self, source, nil, sgs.Player_DiscardPile, reason, true)
-		end]]
-	end,
-	on_effect = function(self, effect)
-		local room = effect.from:getRoom()
-		
 		if room:getCardPlace(self:getEffectiveId()) == sgs.Player_PlaceTable then
-			effect.from:addToPile("#decade", self)
+			--移动至隐藏pile，在UI层面上移出游戏，同时防止中途获得此牌（如 “奸雄”）
+			source:addToPile("#decade", self)
 			local log = sgs.LogMessage()
 			log.type = "$AddToPile"
 			log.card_str = self:toString()
 			log.arg = "single_target_trick"
 			room:sendLog(log)
+			
+			--移动至PlaceUnknown，在服务器层面上移出游戏，防止AI把隐形牌用作视为技（如 隐形“武圣”【杀】），但再次获得此牌时会闪退（如 作弊卡牌一览）
+			local reason = sgs.CardMoveReason(sgs.CardMoveReason_S_REASON_USE, source:objectName(), "", self:getSkillName(), "")
+			room:moveCardTo(self, source, nil, sgs.Player_PlaceUnknown, reason, true)
 		end
 		
+		room:getThread():delay(1700)
+		for _, t in ipairs(targets) do
+			room:cardEffect(self, source, t)
+		end
+	end,
+	on_effect = function(self, effect)
+		local room = effect.from:getRoom()		
 		local x = 2
 		for i = 1, 9, 1 do
 			local ids = room:getNCards(1, false)
@@ -127,6 +127,37 @@ decade = sgs.CreateTrickCard{
 }
 
 decade:clone(0, 10):setParent(extension)
+
+--防止作弊卡牌一览获得移出游戏的牌
+gaodaexcard_skill = sgs.CreateTriggerSkill{
+	name = "gaodaexcard_skill",
+	events = {sgs.BeforeCardsMove},
+	global = true,
+	priority = 3,
+	can_trigger = function(self, target)
+		return true
+	end,
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		local move = data:toMoveOneTime()
+		if move.to and move.to:objectName() == player:objectName() then
+			for _, id in sgs.qlist(move.card_ids) do
+				local card = sgs.Sanguosha:getCard(id)
+				if table.contains({"final_vent", "decade"}, card:objectName()) and room:getCardPlace(id) == sgs.Player_PlaceUnknown then
+					--移出游戏就不能再拿回来，不然会闪退
+					move.card_ids:removeOne(id)
+				else
+					return false
+				end
+			end
+			data:setValue(move)
+		end
+	end
+}
+
+local skills = sgs.SkillList()
+if not sgs.Sanguosha:getSkill("gaodaexcard_skill") then skills:append(gaodaexcard_skill) end
+sgs.Sanguosha:addSkills(skills)
 
 sgs.LoadTranslationTable{
 	["gaodaexcard"] = "高达杀乱入卡",
